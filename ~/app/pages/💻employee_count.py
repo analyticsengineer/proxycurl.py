2d19be0c19573ae93b5d729712181da82012f059
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-10 21:57:57 +0100

    Update 💻employee_count.py

--- ~/app/pages/💻employee_count.py
+++ ~/app/pages/💻employee_count.py
@@ -3,6 +3,7 @@ import streamlit as st
 from PIL import Image
 
 image = Image.open('image2.png')
+st.image(image2)
 
 st.write("Cost: 1 credit / successful request.")
 st.write("Get a number of total employees of a Company.")
