f80ec2f7f62363b41ba6d965b0693751ce5af942
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-10 22:37:14 +0100

    Create company_profile.py

---
+++ ~/app/pages/company_profile.py
@@ -0,0 +1,37 @@
+import requests
+import streamlit as st
+from PIL import Image
+from pathlib import Path
+import json
+
+image = Image.open('image2.png')
+st.image(image)
+
+st.title("Company Profile Picture Endpoint")
+
+st.write("Cost: 0 credit / successful request.")
+st.write("Get the profile picture of a company")
+
+api_endpoint = 'https://nubela.co/proxycurl/api/linkedin/company/profile-picture'
+
+api_key = st.text_input('ENTER YOUR API KEY')
+company = st.text_input('ENTER LINKEDIN COMPANY URL')
+
+if st.button('Get Data'):
+  header_dic = {'Authorization': 'Bearer ' + api_key}
+  params = {
+     'linkedin_company_profile_url': 'https://www.linkedin.com/company/apple/',
+  }
+  response = requests.get(api_endpoint,
+                        params=params,
+                        headers=header_dic)
+
+    st.write(response)
+    
+    with open("profile_image.png", "rb") as file:
+      btn = st.download_button(
+            label="Download image",
+            data=file,
+            file_name="flower.png",
+            mime="image/png"
+          )
