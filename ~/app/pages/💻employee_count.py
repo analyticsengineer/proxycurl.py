25e7d51c2779db2119e53333dd505463742e1172
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-10 21:32:00 +0100

    Update 💻employee_count.py

--- ~/app/pages/💻employee_count.py
+++ ~/app/pages/💻employee_count.py
@@ -3,3 +3,6 @@ import streamlit as st
 
 api_endpoint = 'https://nubela.co/proxycurl/api/linkedin/company/employees/count/'
 api_key = st.text_input('ENTER YOUR API KEY')
+company = st.text_input('ENTER COMPANY LINKEDIN URL')
+
+st.write(requests.get(company).json())
