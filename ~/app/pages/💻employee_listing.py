931e3e4fcc903ffacaaf63ea5ebb0ca50affcac4
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-11 02:02:21 +0100

    Update 💻employee_listing.py

--- ~/app/pages/💻employee_listing.py
+++ ~/app/pages/💻employee_listing.py
@@ -14,7 +14,7 @@ st.write("Get a list of employees of a Company.")
 st.write("This API endpoint is limited by LinkDB which is populated with profiles in the US, UK, Canada, Israel, Australia, Ireland, New Zealand and Singapore." 
          "As such, this endpoint is best used to list employees working in companies based in those locations only.")
 
-api_endpoint = 'https://nubela.co/proxycurl/api/linkedin/company/employees/count/'
+api_endpoint = 'https://nubela.co/proxycurl/api/linkedin/company/employees/'
 
 api_key = st.text_input('ENTER YOUR API KEY')
 company = st.text_input('ENTER LINKEDIN COMPANY URL')
@@ -22,14 +22,16 @@ company = st.text_input('ENTER LINKEDIN COMPANY URL')
 if st.button('Get Data'):
     header_dic = {'Authorization': 'Bearer ' + api_key}
     params = {
-        'linkedin_employee_count': 'include',
+        'enrich_profiles': 'enrich',
+        'role_search': '[Ff][Oo][Uu][Nn][Dd][Ee][Rr]',
+        'page_size': '100',
         'employment_status': 'current',
+        'resolve_numeric_id': 'false',
         'url': company,
     }
     response = requests.get(api_endpoint,
                         params=params,
                         headers=header_dic)
-   
 
     st.write(response.json())
     
