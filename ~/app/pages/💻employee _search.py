1bb1e0cf3148c080f0aa093db2d69dd69e38fb53
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-11 04:15:48 +0100

    Update 💻employee _search.py

--- ~/app/pages/💻employee _search.py
+++ ~/app/pages/💻employee _search.py
@@ -29,21 +29,3 @@ if st.button('Get Data'):
                         headers=header_dic)
 
     st.write(response.json())
-    
-    jsonFile = open("response.json", "w")
-    #jsonFile.write(jsonString)
-    jsonFile.close()
-
-    if st.button('Convert To Csv File'):
-        with open('response.json', 'r') as f:
-            data = json.load(f)
-            df = pd.json_normalize(data)
-            df.to_csv('employee_search_point.csv', encoding='utf-8', index=False)
-            df = pd.DataFrame(df)
-            file_name = "employee_end_point.csv"
-            file_path = f"./{file_name}"
-            df = open(file_path, 'rb')
-            st.download_button(label='Click to download',
-                      data=df,
-                      file_name=file_name,
-                      key='download_df')
