679a815389edfe18e1e7a69d0487698f99ee00c0
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-11 02:24:32 +0100

    Create company_profile.py

---
+++ ~/app/pages/company_profile.py
@@ -0,0 +1,53 @@
+import requests
+import streamlit as st
+from PIL import Image
+import json
+
+image = Image.open('image2.png')
+st.image(image)
+
+st.title("Company Profile Endpoint")
+
+st.write("Cost: 1 credit / successful request.")
+st.write("Get structured data of a Company Profile")
+
+api_endpoint = 'https://nubela.co/proxycurl/api/linkedin/company'
+
+api_key = st.text_input('ENTER YOUR API KEY')
+company = st.text_input('ENTER COMPANY URL')
+
+if st.button('Get Data'):
+    header_dic = {'Authorization': 'Bearer ' + api_key}
+    params = {
+       'resolve_numeric_id': 'true',
+       'categories': 'include',
+       'funding_data': 'include',
+       'extra': 'include',
+       'exit_data': 'include',
+       'acquisitions': 'include',
+       'url': company,
+       'use_cache': 'if-present',
+    }
+    response = requests.get(api_endpoint,
+                        params=params,
+                        headers=header_dic)
+
+    st.write(response.json())
+    
+    jsonFile = open("response.json", "w")
+    #jsonFile.write(jsonString)
+    jsonFile.close()
+
+    if st.button('Convert To Csv File'):
+        with open('response.json', 'r') as f:
+            data = json.load(f)
+            df = pd.json_normalize(data)
+            df.to_csv('company_profile_endpoint.csv', encoding='utf-8', index=False)
+            df = pd.DataFrame(df)
+            file_name = "company_profile_endpoint.csv"
+            file_path = f"./{file_name}"
+            df = open(file_path, 'rb')
+            st.download_button(label='Click to download',
+                      data=df,
+                      file_name=file_name,
+                      key='download_df')
