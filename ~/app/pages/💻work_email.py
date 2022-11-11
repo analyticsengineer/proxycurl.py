a69f7d476af025960feec479f7420b82ec24f86d
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-11 03:04:12 +0100

    Create 💻work_email.py

---
+++ ~/app/pages/💻work_email.py
@@ -0,0 +1,31 @@
+import requests
+import streamlit as st
+from PIL import Image
+import json
+
+image = Image.open('image2.png')
+st.image(image)
+
+st.title("Work Email Lookup Endpoint")
+
+st.write("Cost: 3 credits / request.")
+st.write("Lookup work email address of a LinkedIn Person Profile.")
+st.write("Email addresses returned are verified to not be role-based or catch-all emails."
+         "Email addresses returned by our API endpoint come with a 95+% deliverability guarantee")
+
+api_endpoint = 'https://nubela.co/proxycurl/api/linkedin/profile/email'
+
+api_key = st.text_input('ENTER YOUR API KEY')
+profile = st.text_input('ENTER YOUR LINKEDIN PROFILE URL')
+
+if st.button('Get Data'):
+    header_dic = {'Authorization': 'Bearer ' + api_key}
+    params = {
+      'linkedin_profile_url': profile,
+    }
+    response = requests.get(api_endpoint,
+                        params=params,
+                        headers=header_dic)
+    st.write(response.json())
+    
+   
