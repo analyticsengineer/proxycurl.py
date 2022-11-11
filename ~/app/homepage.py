363b49ae93c0e37608cb6a4751964eb3c780964f
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-11 01:55:25 +0100

    Update homepage.py

--- ~/app/homepage.py
+++ ~/app/homepage.py
@@ -7,7 +7,7 @@ image = Image.open('image.png')
 col1, col2 = st.columns(2)
 
 col1.header("Proxy Curl - Linkedin API")
-title_1 =  '<p style="font-family:sans-serif; color:White;">This Web App is built for,</p>'
+title_1 =  '<p style="font-family:sans-serif; color:White;">This Web App is built for</p>'
 col1.markdown(title_1, unsafe_allow_html=True)
 title_2 =  '<p style="font-family:sans-serif; color:White;">Non programmers who want to use Proxycurl API.</p>'
 col1.markdown(title_2, unsafe_allow_html=True)
