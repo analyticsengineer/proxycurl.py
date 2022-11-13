8ecd1d2deffad4114c6c134eefcf041f81261ce9
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-13 02:43:58 +0100

    Update Homepage.py

--- ~/app/Homepage.py
+++ ~/app/Homepage.py
@@ -8,7 +8,7 @@ image2 = Image.open('proxycurl.png')
 col1, col2 = st.columns(2)
 col1.image(image2)
 
-col1.header("Proxy Curl - Linkedin API")
+col1.title("Proxy Curl - Linkedin API")
 title_1 =  '<p style="font-family:sans-serif; color:White;">This Web App is built for Non programmers</p>'
 col1.markdown(title_1, unsafe_allow_html=True)
 title_2 =  '<p style="font-family:sans-serif; color:White;">Who would like to use Proxycurl API.</p>'
