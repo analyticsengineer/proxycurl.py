4c6c7c130861e7604afa65049b55e84fa6e9ee88
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-13 01:27:35 +0100

    Update and rename 💻employee_listing.py to 🏆Employee_Listing.py

--- ~/app/pages/🏆Employee_Listing.py
+++ ~/app/pages/🏆Employee_Listing.py
@@ -15,10 +15,10 @@ st.write("This API endpoint is limited by LinkDB which is populated with profile
 
 api_endpoint = 'https://nubela.co/proxycurl/api/linkedin/company/employees/'
 
-api_key = st.text_input('ENTER YOUR API KEY')
-company = st.text_input('ENTER LINKEDIN COMPANY URL')
+api_key = st.text_input('Enter your api key')
+company = st.text_input('Enter company LinkIn Url')
 
-if st.button('Get Data'):
+if st.button('📥'):
     header_dic = {'Authorization': 'Bearer ' + api_key}
     params = {
         'enrich_profiles': 'enrich',
@@ -34,17 +34,15 @@ if st.button('Get Data'):
 
     st.write(response.json())
     
-    jsonFile = open("response.json", "w")
-    #jsonFile.write(jsonString)
-    jsonFile.close()
-
-    if st.button('Convert To Csv File'):
+    if st.button('📥 to csv'):
+        jsonFile = open('response.json', 'w')
+        jsonFile.close()
         with open('response.json', 'r') as f:
             data = json.load(f)
             df = pd.json_normalize(data)
-            df.to_csv('employee_listing_endpoint.csv', encoding='utf-8', index=False)
+            df.to_csv('company_profile_endpoint.csv', encoding='utf-8', index=False)
             df = pd.DataFrame(df)
-            file_name = "employee_listing_endpoint.csv"
+            file_name = 'company_profile_endpoint.csv'
             file_path = f"./{file_name}"
             df = open(file_path, 'rb')
             st.download_button(label='Click to download',
