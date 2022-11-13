9bffa6b7ecb843ba47db884e36d8bf7d3a8eb7b1
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-13 01:29:23 +0100

    Update and rename 💻job_listing.py to 💻Job_Listing.py

--- ~/app/pages/💻Job_Listing.py
+++ ~/app/pages/💻Job_Listing.py
@@ -16,11 +16,11 @@ st.write("The search_id of the company on LinkedIn."
 
 api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin/company/job'
 
-api_key = st.text_input('ENTER YOUR API KEY')
-search = st.text_input('ENTER COMPANY LINKEDIN SEARCH ID')
+api_key = st.text_input('Enter your api key')
+search = st.text_input('Enter LinkedIn company search Id')
 
 
-if st.button('Get Data'):
+if st.button('📥'):
     header_dic = {'Authorization': 'Bearer ' + api_key}
     params = {
       'search_id': search,
@@ -31,17 +31,15 @@ if st.button('Get Data'):
 
     st.write(response.json())
     
-    jsonFile = open("response.json", "w")
-    #jsonFile.write(jsonString)
-    jsonFile.close()
-
-    if st.button('Convert To Csv File'):
+    if st.button('📥 to csv'):
+        jsonFile = open('response.json', 'w')
+        jsonFile.close()
         with open('response.json', 'r') as f:
             data = json.load(f)
             df = pd.json_normalize(data)
-            df.to_csv('job_listing.csv', encoding='utf-8', index=False)
+            df.to_csv('company_profile_endpoint.csv', encoding='utf-8', index=False)
             df = pd.DataFrame(df)
-            file_name = "job_listing.csv"
+            file_name = 'company_profile_endpoint.csv'
             file_path = f"./{file_name}"
             df = open(file_path, 'rb')
             st.download_button(label='Click to download',
