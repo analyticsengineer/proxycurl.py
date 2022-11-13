7dd02a429248f6c381146ff98809bc73ab674e1e
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-13 02:24:24 +0100

    Update Homepage.py

--- ~/app/Homepage.py
+++ ~/app/Homepage.py
@@ -13,5 +13,9 @@ title_2 =  '<p style="font-family:sans-serif; color:White;">Who would like to us
 col1.markdown(title_2, unsafe_allow_html=True)
 col2.image(image)
 
-col1.write("[![Project Page](https://img.icons8.com/material-outlined/24/undefined/github.png)](https://github.com/anuoluwapods/proxycurl.py)") col1.write("[![Instagram page](https://img.icons8.com/fluency/20/null/instagram-new.png)](https://instagram.com/anuoluwapods)")
+col1.write("[![Project Page](https://img.icons8.com/material-outlined/24/undefined/github.png)](https://github.com/anuoluwapods/proxycurl.py)") 
+col1.write("[![Instagram page](https://img.icons8.com/fluency/20/null/instagram-new.png)](https://instagram.com/anuoluwapods)")
+col1.write("[![Instagram page](https://img.icons8.com/color/20/null/twitter--v1.png)](https://twitter.com/AnuoluwapoDs)")
+col1.write("[![Instagram page](https://img.icons8.com/fluency/20/null/instagram-new.png)](https://instagram.com/anuoluwapods)")
+col1.write("[![Instagram page](https://img.icons8.com/fluency/20/null/instagram-new.png)](https://instagram.com/anuoluwapods)")
 
