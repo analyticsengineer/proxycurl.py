60decb3a5b5d2c368ba3a05c17acc036f0fcba7c
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-13 02:27:28 +0100

    Update Homepage.py

--- ~/app/Homepage.py
+++ ~/app/Homepage.py
@@ -16,6 +16,6 @@ col2.image(image)
 col1.write("[![Project Page](https://img.icons8.com/material-outlined/24/undefined/github.png)](https://github.com/anuoluwapods/proxycurl.py)") 
 col1.write("[![Instagram page](https://img.icons8.com/fluency/20/null/instagram-new.png)](https://instagram.com/anuoluwapods)")
 col1.write("[![Instagram page](https://img.icons8.com/color/20/null/twitter--v1.png)](https://twitter.com/AnuoluwapoDs)")
-col1.write("[![Instagram page](https://img.icons8.com/fluency/20/null/instagram-new.png)](https://instagram.com/anuoluwapods)")
-col1.write("[![Instagram page](https://img.icons8.com/fluency/20/null/instagram-new.png)](https://instagram.com/anuoluwapods)")
+col1.write("[![Instagram page](https://img.icons8.com/color/20/null/twitter--v1.png)](https://www.linkedin.com/in/anuoluwapo-abiodun-balogun-64b977186/)")
+col1.write("[![Instagram page](https://img.icons8.com/fluency/20/null/facebook-new.png)](https://facebook.com/ifeoluwapo.balogun)")
 
