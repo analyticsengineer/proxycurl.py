a1ebfa99260de63806d9cedf5e699f068ef2658d
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-13 02:44:37 +0100

    Update Homepage.py

--- ~/app/Homepage.py
+++ ~/app/Homepage.py
@@ -6,14 +6,15 @@ image = Image.open('image.png')
 image2 = Image.open('proxycurl.png')
 
 col1, col2 = st.columns(2)
-col1.image(image2)
 
-col1.title("Proxy Curl - Linkedin API")
+
+col1.Header("Proxy Curl - Linkedin API")
 title_1 =  '<p style="font-family:sans-serif; color:White;">This Web App is built for Non programmers</p>'
 col1.markdown(title_1, unsafe_allow_html=True)
 title_2 =  '<p style="font-family:sans-serif; color:White;">Who would like to use Proxycurl API.</p>'
 col1.markdown(title_2, unsafe_allow_html=True)
 col2.image(image)
+col2.image(image2)
 
 col1.write("[![Project Page](https://img.icons8.com/ios-glyphs/20/ffffff/github.png)](https://github.com/anuoluwapods/proxycurl.py) Github Page") 
 col1.write("[![Instagram page](https://img.icons8.com/fluency/20/null/instagram-new.png)](https://instagram.com/anuoluwapods) Instagram page")
