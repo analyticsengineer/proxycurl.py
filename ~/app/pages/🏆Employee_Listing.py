7d5466e4ef733fdd52db69ad992bcc8f5c6a026e
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2022-11-14 01:39:53 +0100

    Update 🏆Employee_Listing.py

--- ~/app/pages/🏆Employee_Listing.py
+++ ~/app/pages/🏆Employee_Listing.py
@@ -32,17 +32,16 @@ if st.button('📥'):
                         params=params,
                         headers=header_dic)
 
-    st.write(response.json())
+    respond = st.write(response.json())
+    st.write(respond)
     
     if st.button('📥 to csv'):
-        jsonFile = open('response.json', 'w')
-        jsonFile.close()
-        with open('response.json', 'r') as f:
-            data = json.load(f)
-            df = pd.json_normalize(data)
-            df.to_csv('company_profile_endpoint.csv', encoding='utf-8', index=False)
+        with open('employeelisting.json', 'w') as outfile:
+            json.dump(respond, outfile)
+            df = pd.read_json ('employeelisting.json')
+            df.to_csv ('employeelisting.csv', encoding='utf-8', index=False)
             df = pd.DataFrame(df)
-            file_name = 'company_profile_endpoint.csv'
+            file_name = 'employeelisting.csv'
             file_path = f"./{file_name}"
             df = open(file_path, 'rb')
             st.download_button(label='Click to download',
